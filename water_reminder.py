bd7c824bc46185f7b13fe3daeaa28c6291e035f8
Tsaitzu <tsaitzu21@gmail.com>
2026-01-24 01:50:40 +0800

    Add files via upload

---
+++ water_reminder.py
@@ -0,0 +1,8 @@
+# water reminder for practice only
+# while loop
+
+water_drank = int(input('How much water did you drink? '))
+
+while water_drank < 2000:
+    water_drank += 500
+    print(f"Current water intake: {water_drank}ml")
